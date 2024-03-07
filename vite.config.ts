54b6a1df97dcc15d079a77777906e890e9376745
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2024-03-07 00:15:28 -0300

    Add million package and configure Vite for million compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import million from "million/compiler";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    million.vite({ auto: true }),
     remix({
       ssr: false,
     }),
